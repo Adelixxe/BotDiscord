46a5a4ad9690fc5e23130e7507162df53c4732f8
Adelixxxe <37460087+Adelixxxe@users.noreply.github.com>
2019-03-25 00:54:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -27,12 +27,12 @@ bot.on('message', message => {
             })
 
             .then((collected) => {
-                message.react("👌")
                 maximum = collected.first().content
                 j = Math.floor(Math.random() * 11);
                 if (j % 2 == 0) {
                     i = Math.floor((Math.random() * maximum) + 1);
                     console.log(i);
+                message.react("👌")                   
                 message.channel.send("https://media3.giphy.com/media/1O1Xww2UM0VMhROihc/giphy.gif?cid=3640f6095c9812507957636a2e884db8")
                 message.reply([i])
                 maximum = 0
