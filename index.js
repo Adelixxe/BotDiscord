7104c8e8b85e7f3c96b44954355ca6aab27c59e0
Adelixxxe <37460087+Adelixxxe@users.noreply.github.com>
2019-03-25 01:07:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client();
 
 var cli = new Discord.Client({autoReconnect:true});
 var maximum = 0;
-
+var check = false;
 bot.commands = new Discord.Collection();
 bot.on("error", (e) => console.error(e));
 bot.on("warn", (e) => console.warn(e));
@@ -17,7 +17,7 @@ bot.on('ready', () => {
 });
 
 bot.on('message', message => {
-    if (message.content === "!jdes") {
+    if ((message.content === "!jdes") && (check === false) ) {
         message.channel.send("Quel type de dès veux tu jeter ? (3,4,6,8,10,12,16,20,24,30,100)")
         .then(() => {
             message.channel.awaitMessages(() => true, {
@@ -27,27 +27,27 @@ bot.on('message', message => {
             })
 
             .then((collected) => {
+                check = true
                 maximum = collected.first().content
-                j = Math.floor(Math.random() * 11);
-                if (j % 2 == 0) {
-                    i = Math.floor((Math.random() * maximum) + 1);
-                    console.log(i);
-                message.react("👌")                   
-                message.channel.send("https://media3.giphy.com/media/1O1Xww2UM0VMhROihc/giphy.gif?cid=3640f6095c9812507957636a2e884db8")
-                message.reply([i])
-                maximum = 0
-                    }
-                })
-                .catch(() => {
-                    message.channel.send("Il y a eu un petit problème :/");
-          });;
-            
-            })
+
             .catch(() => {
                 message.channel.send("Trop tard.");
+                check = false
+            })
+            });
       });
-        
-        }            
+        if(check = true){      
+            j = Math.floor(Math.random() * 11);
+            if (j % 2 == 0) {
+                i = Math.floor((Math.random() * maximum) + 1);
+                console.log(i);              
+            message.channel.send("https://media3.giphy.com/media/1O1Xww2UM0VMhROihc/giphy.gif?cid=3640f6095c9812507957636a2e884db8")
+            message.reply([i])
+            maximum = 0
+            check = false
+                } 
+            } 
+        }           
     })
 
 
